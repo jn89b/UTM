a3c78bacae29d901da15617e8058fc7bb7027660
Justin Nguyen <jnguyenblue2804@gmail.com>
2021-10-07 19:45:23 -0500

    added test package

--- utm/tests/mongo_recieve_info.py
+++ utm/tests/mongo_recieve_info.py
@@ -14,6 +14,7 @@ else:
     import StringIO
 
 """
+DB NODE
 listens to incoming uav id 
 if uav id does not exist in database:
     request query of uav information
@@ -27,6 +28,8 @@ UAV information recorded in database: DONE
     -uav battery
     -uav service request
     -uav state
+    =bundle with id number
+
 
 third party service sees what information it requests 
 if it requests the service then will publish information/control 
@@ -73,31 +76,37 @@ if __name__ == '__main__':
             print("item is:", item)
             some_list.append(item)
         """
+        p = Pose(Point(0, 1, 2), Quaternion(3, 4,  5, 6))
 
         for item,meta in msg_store.query_named("uav0", StringPairList._type, single=False):
             #print(item._id)
             """delete information of drone"""
             #print(item)
             pose_id = item.pairs[0].second
-            pose_val = msg_store.query_id(item.pairs[0].second, Pose._type)
- 
             point_id = item.pairs[1].second
             quat_id = item.pairs[2].second
             result_id = item.pairs[3].second
+            #retrieve pose value
+            pose_val = msg_store.query_id(item.pairs[0].second, Pose._type)
             
+            #update pose of uav
+            msg_store.update_id(pose_id, p)
+
             some_list.append(item)
 
             """delete ids for pose_id based on uav value"""
+            """
             msg_store.delete(pose_id)
             msg_store.delete(point_id)
             msg_store.delete(quat_id)
 
             msg_store.delete(result_id)
-
+            """
+            
             """delete id bundle key of drone"""
             #print(meta.get('_id'))
-            id_num = meta.get('_id')
-            msg_store.delete(str(id_num))
+            #id_num = meta.get('_id')
+            #msg_store.delete(str(id_num))
 
             #print(item.query_id)
 
