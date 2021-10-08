547dac5d61278f911259d0e196fe4fbb2e1b3c2d
Justin Nguyen <jnguyenblue2804@gmail.com>
2021-10-07 19:12:32 -0500

    included test script to remove uav from db

--- utm/scripts/mongo_db_test.py
+++ utm/scripts/mongo_db_test.py
@@ -12,71 +12,96 @@ if float(platform.python_version()[0:2]) >= 3.0:
 else:
     import StringIO
 
+"""
+listens to incoming uav id 
+if uav id does not exist in database:
+    request query of uav information
+    only update the uav position 
+else uav id does not exist in database:
+    log new information about uav
+
+UAV information recorded in database: DONE
+    -uav id
+    -uav position information, global and local
+    -uav battery
+    -uav service request
+    -uav state
+
+third party service sees what information it requests 
+if it requests the service then will publish information/control 
+of drone
+
+Be able to remove UAV from database:
+    -request uav id number
+    -get all documents of uav id number
+    -remove all from database
+
+"""
 
 if __name__ == '__main__':
 
     rospy.init_node("example_message_store_client")
 
+    rate = rospy.Rate(0.5)
+
     msg_store = MessageStoreProxy()
 
     p = Pose(Point(0, 1, 2), Quaternion(3, 4,  5, 6))
 
-    try:
+    while not rospy.is_shutdown():
+        try:
 
-        # insert a pose object with a name, store the id from db
-        p_id = msg_store.insert_named("uav0", p)
+            # insert a pose object with a name, store the id from db
+            p_id = msg_store.insert_named("uav0", p)
 
-        # you don't need a name (note that this p_id is different than one above)
-        p_id = msg_store.insert(p)
+            # this is how you retrieve a query get it back with a name
+            print(msg_store.query_named("uav0", Pose._type))
 
-        #p_id = msg_store.insert(['test1', 'test2'])
+            p.position.x = 666
 
-        # this is how you retrieve a query get it back with a name
-        print(msg_store.query_named("uav0", Pose._type))
+            # update it with a name
+            #msg_store.update_named("hello world", p)
 
-        p.position.x = 666
+            p.position.y = 2020
 
-        # update it with a name
-        msg_store.update_named("hello world", p)
+            # update the other inserted one using the id
+            msg_store.update_id(p_id, p)    
 
-        p.position.y = 2020
+            stored_p, meta = msg_store.query_id(p_id, Pose._type)
 
-        # update the other inserted one using the id
-        msg_store.update_id(p_id, p)
+            assert stored_p.position.x == 666
+            assert stored_p.position.y == 2020
+        
+            """QUERYS"""
+            print("stored object ok")
+            print("stored object inserted at %s (UTC rostime) by %s" % (meta['inserted_at'], meta['inserted_by']))
+            print("stored object last updated at %s (UTC rostime) by %s" % (meta['last_updated_at'], meta['last_updated_by']))
 
-        stored_p, meta = msg_store.query_id(p_id, Pose._type)
+            # get it back with a name
+            print(msg_store.query_named("uav0", Pose._type))
 
-        assert stored_p.position.x == 666
-        assert stored_p.position.y == 2020
-     
-        """QUERYS"""
-        print("stored object ok")
-        print("stored object inserted at %s (UTC rostime) by %s" % (meta['inserted_at'], meta['inserted_by']))
-        print("stored object last updated at %s (UTC rostime) by %s" % (meta['last_updated_at'], meta['last_updated_by']))
+            # try to get it back with an incorrect name, so get None instead
+            print(msg_store.query_named("my favourite position", Pose._type))
 
-        # get it back with a name
-        print(msg_store.query_named("my favourite pose", Pose._type))
+            # get all poses
+            print(msg_store.query(Pose._type))
 
-        # try to get it back with an incorrect name, so get None instead
-        print(msg_store.query_named("my favourite position", Pose._type))
+            # get the latest one pose
+            print(msg_store.query(Pose._type, sort_query=[("$natural", -1)], single=True))
 
-        # get all poses
-        print(msg_store.query(Pose._type))
+            # get all non-existant typed objects, so get an empty list back
+            print(msg_store.query( "not my type"))
 
-        # get the latest one pose
-        print(msg_store.query(Pose._type, sort_query=[("$natural", -1)], single=True))
+            # get all poses where the y position is 1
+            print(msg_store.query(Pose._type, {"position.y": 1}))
 
-        # get all non-existant typed objects, so get an empty list back
-        print(msg_store.query( "not my type"))
+            # get all poses where the y position greater than 0
+            print(msg_store.query(Pose._type, {"position.y": {"$gt": 0}}))
 
-        # get all poses where the y position is 1
-        print(msg_store.query(Pose._type, {"position.y": 1}))
+            rate.sleep()
 
-        # get all poses where the y position greater than 0
-        print(msg_store.query(Pose._type, {"position.y": {"$gt": 0}}))
-
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
 
 
 
---
+++ utm/scripts/mongo_recieve_info.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import rospy
+import mongodb_store_msgs.srv as dc_srv
+import mongodb_store.util as dc_util
+from mongodb_store.message_store import MessageStoreProxy
+from mongodb_store_msgs.msg import StringPairList, StringPair
+from geometry_msgs.msg import Pose, Point, Quaternion
+import platform
+if float(platform.python_version()[0:2]) >= 3.0:
+    import io
+else:
+    import StringIO
+
+"""
+listens to incoming uav id 
+if uav id does not exist in database:
+    request query of uav information
+    only update the uav position 
+else uav id does not exist in database:
+    log new information about uav
+
+UAV information recorded in database: DONE
+    -uav id
+    -uav position information, global and local
+    -uav battery
+    -uav service request
+    -uav state
+
+third party service sees what information it requests 
+if it requests the service then will publish information/control 
+of drone
+
+Be able to remove UAV from database: DONE
+    -request uav id number
+    -get all documents of uav id number
+    -remove all from database
+
+http://docs.ros.org/en/kinetic/api/mongodb_store/html/message_store_proxy.html
+"""
+
+if __name__ == '__main__':
+
+    rospy.init_node("recieve_db")
+
+    rate = rospy.Rate(0.5)
+
+    msg_store = MessageStoreProxy(collection='pose_results')
+
+    try:
+        """get all information about uav"""
+        #result = msg_store.query(StringPairList._type)
+        single = msg_store.query_named("uav0", Pose._type)
+        #print("single is:", single)
+
+        #result = msg_store.query(StringPairList._type)
+        #multiple = msg_store.query(StringPairList._type,{"name": {"$meta": "uav0"}})
+        #print("multiple:", multiple)
+        some_list = []
+        i = 0
+        """
+        test query
+        for item in msg_store.query(Pose._type):
+            print("item is:", item)
+            some_list.append(item)
+            i = i + 1
+            #print(item)
+        """
+        """
+        get all values of uav set single = False
+        for item in msg_store.query_named("uav0", Pose._type,single=False):
+            print("item is:", item)
+            some_list.append(item)
+        """
+
+        for item,meta in msg_store.query_named("uav0", StringPairList._type, single=False):
+            #print(item._id)
+            """delete information of drone"""
+            #print(item)
+            pose_id = item.pairs[0].second
+            pose_val = msg_store.query_id(item.pairs[0].second, Pose._type)
+ 
+            point_id = item.pairs[1].second
+            quat_id = item.pairs[2].second
+            result_id = item.pairs[3].second
+            
+            some_list.append(item)
+
+            """delete ids for pose_id based on uav value"""
+            msg_store.delete(pose_id)
+            msg_store.delete(point_id)
+            msg_store.delete(quat_id)
+
+            msg_store.delete(result_id)
+
+            """delete id bundle key of drone"""
+            #print(meta.get('_id'))
+            id_num = meta.get('_id')
+            msg_store.delete(str(id_num))
+
+            #print(item.query_id)
+
+        
+        print(len(some_list))
+        #print(some_list)
+
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+
+
